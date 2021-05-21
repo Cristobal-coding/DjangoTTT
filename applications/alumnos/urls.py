a2e74b0da84ca6c2713976a312f8debc34df71a3
Cristobal-Coding <agartkg95@gmail.com>
2021-05-21 16:31:51 -0600

    making url and template

--- applications/alumnos/urls.py
+++ applications/alumnos/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
-# from .views import LoginPage, LogoutView
+from .views import AlumnosHome
 app_name = 'alumnos_app'
 
 urlpatterns = [
-    # path('alumnos/', views.HomePage.as_view(), name='alumnos'),
+    path('alumnos/', views.AlumnosHome.as_view(), name='inicio'),
 
 ]
