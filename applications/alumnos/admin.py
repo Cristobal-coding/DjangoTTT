f5511e630e0f06222de98815cd7724765cf83333
nattohd <tubepika@gmail.com>
2021-05-21 18:09:01 -0400

    models

--- applications/alumnos/admin.py
+++ applications/alumnos/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from .models import Apoderado, Alumno
 # Register your models here.
+admin.site.register(Apoderado)
+admin.site.register(Alumno)
--- applications/alumnos/models.py
+++ applications/alumnos/models.py
@@ -1,3 +1,31 @@
 from django.db import models
-
-# Create your models here.
+class Apoderado(models.Model):
+    rut =models.CharField('Rut',max_length=13,unique=True,primary_key=True)    
+    nombre_apoderado=models.CharField('Nombre apoderado',max_length=30)
+    telefono_apoderado=models.CharField('Telefono apoderado',max_length=15)
+    correo=models.CharField('Correo',max_length=20)
+    def __str__(self):
+        return self.rut
+    class Meta:
+        verbose_name = 'Apoderado'
+        verbose_name_plural = 'Apoderados'
+        db_table= 'Apoderados'
+class Alumno(models.Model):
+    SEX_CHOICES = (
+        ('0','Femenino'),
+        ('1','Masculino'),
+        ('2','No especifica')
+    )
+    rut =models.CharField('Rut',max_length=13,unique=True,primary_key=True)
+    nombre = models.CharField('Nombre',max_length=15,unique=False)
+    apellido_paterno =models.CharField('Apellido paterno',max_length=15)
+    apellido_materno =models.CharField('Apellido materno',max_length=15)
+    fecha_nacimiento=models.DateField('Fecha de nacimiento')
+    rut_apoderado=models.ForeignKey(Apoderado,on_delete=models.CASCADE,max_length=13,unique=False)
+    sexo=models.CharField('Sexo',max_length=1,choices=SEX_CHOICES)
+    telefono=models.CharField('Telefono',max_length=15)
+    direccion=models.CharField('Direccion',max_length=20)
+    class Meta:
+        verbose_name = 'Alumno'
+        verbose_name_plural = 'Alumnos'
+        db_table= 'Alumnos'
