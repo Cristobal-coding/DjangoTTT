31237345f09f971c7df7bb5c75869180fbbbcfda
Cristobal-Coding <agartkg95@gmail.com>
2021-05-22 16:11:40 -0600

    make a url

--- applications/alumnos/urls.py
+++ applications/alumnos/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
-from .views import AlumnosHome
+from .views import AlumnosHome, AlumnosFiltros
 app_name = 'alumnos_app'
 
 urlpatterns = [
     path('alumnos/', views.AlumnosHome.as_view(), name='inicio'),
+    path('filtros/', views.AlumnosFiltros.as_view(), name='filtrar'),
 
 ]
--- applications/alumnos/views.py
+++ applications/alumnos/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView , ListView
 # Create your views here.
+from applications.alumnos.models import Alumno
 
 class AlumnosHome(TemplateView):
     template_name = 'alumnos/inicio.html'
+
+class AlumnosFiltros(ListView):
+    template_name = 'alumnos/filtros.html'
+    model = Alumno
+    context_object_name = 'alumnos'
