5c9944f08616bb12d9a8bad04b3de1cc23861d0a
nattohd <tubepika@gmail.com>
2021-05-22 20:23:36 -0400

    modelss

--- applications/alumnos/models.py
+++ applications/alumnos/models.py
@@ -2,9 +2,9 @@ from django.db import models
 class Apoderado(models.Model):
     rut =models.CharField('Rut',max_length=13,unique=True,primary_key=True)    
     nombre_apoderado=models.CharField('Nombre apoderado',max_length=50)
-    apellido_paterno =models.CharField('Apellido paterno',max_length=20)
-    apellido_materno =models.CharField('Apellido materno',max_length=20)
-    telefono_apoderado=models.CharField('Telefono apoderado',max_length=15)
+    apellido_paterno =models.CharField('Apellido paterno',max_length=25)
+    apellido_materno =models.CharField('Apellido materno',max_length=25)
+    telefono_apoderado=models.CharField('Telefono apoderado',max_length=8)
     correo=models.CharField('Correo',max_length=20)
     def __str__(self):
         return self.rut
@@ -20,18 +20,18 @@ class Alumno(models.Model):
     )
     ESTADO_CHOICES=(
         ('0','Regular'),
-        ('1','Se fué'),
+        ('1','Abandonó'),
         ('2','Finalizado')
     )
     rut =models.CharField('Rut',max_length=13,unique=True,primary_key=True)
     nombre = models.CharField('Nombre',max_length=50,unique=False)
-    apellido_paterno =models.CharField('Apellido paterno',max_length=20)
-    apellido_materno =models.CharField('Apellido materno',max_length=20)
+    apellido_paterno =models.CharField('Apellido paterno',max_length=25)
+    apellido_materno =models.CharField('Apellido materno',max_length=25)
     fecha_nacimiento=models.DateField('Fecha de nacimiento')
     rut_apoderado=models.ForeignKey(Apoderado,on_delete=models.CASCADE,max_length=13,unique=False)
     sexo=models.CharField('Sexo',max_length=1,choices=SEX_CHOICES)
-    telefono=models.CharField('Telefono',max_length=15)
-    direccion=models.CharField('Direccion',max_length=20)
+    telefono=models.CharField('Telefono',max_length=8)
+    direccion=models.CharField('Direccion',max_length=50)
     estado=models.CharField('estado',max_length=1,choices=ESTADO_CHOICES)
     class Meta:
         verbose_name = 'Alumno'
