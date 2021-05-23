1d48bc5fc3a0719ba7299112df22352b95c32a38
Cristobal-Coding <agartkg95@gmail.com>
2021-05-22 20:57:31 -0600

    new pull

--- applications/alumnos/views.py
+++ applications/alumnos/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy, reverse
 from django.views.generic import TemplateView , ListView
 # Create your views here.
 from applications.alumnos.models import Alumno
-
-class AlumnosHome(TemplateView):
+from django.contrib.auth.mixins import LoginRequiredMixin
+class AlumnosHome(LoginRequiredMixin,TemplateView):
     template_name = 'alumnos/inicio.html'
-
-class AlumnosFiltros(ListView):
+    login_url = reverse_lazy('home_app:login')
+class AlumnosFiltros(LoginRequiredMixin,ListView):
     template_name = 'alumnos/filtros.html'
     model = Alumno
     context_object_name = 'alumnos'
     paginate_by=4
+    login_url = reverse_lazy('home_app:login')
+    
     
