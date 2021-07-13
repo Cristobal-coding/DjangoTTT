21cbf81476fef49c8a478cd24e16d78d5cd990c9
nattohd <tubepika@gmail.com>
2021-07-13 18:29:55 -0400

    asda

--- applications/antecedentes/views.py
+++ applications/antecedentes/views.py
@@ -1,4 +1,6 @@
-from django.urls.base import reverse_lazy
+from django.contrib import messages
+from django.http.response import HttpResponseRedirect
+from django.urls.base import reverse, reverse_lazy
 from applications.antecedentes.models import Antecedente
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
@@ -10,3 +12,14 @@ class AntecedentesView(LoginRequiredMixin,ListView):
     model = Antecedente
     context_object_name = 'antecedentes'
     login_url = reverse_lazy('home_app:login')   
+
+
+
+def antecedente_create(request):
+    if request.method == 'POST':
+
+        Antecedente.objects.create(
+            nombre_antecedente=request.POST['nombre'],
+        )
+        messages.success(request,'!!Antecedente añadido con exito!!')
+    return HttpResponseRedirect(reverse('antecedentes_app:inicio'))
